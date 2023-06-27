eadae6bf67c7429c14c3b92ad6136a6dc8762bf6
Leandro Doliri <leandoliri@gmail.com>
2023-06-27 00:37:05 -0300

    cambios en favs

--- src/Components/GifCard/GifCard.jsx
+++ src/Components/GifCard/GifCard.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "wouter";
 import "./GifCard.css";
 import { BtnAddToFavs, BtnDeleteFromFavs } from "../Buttons/Buttons";
 import { useFavs } from "../../hooks/useFavs";
@@ -13,20 +14,22 @@ export function GifCard({ gif }) {
 
   const isInFavs = checkGifInFavs({ id: gif.id });
 
+  const [path, pushLocation] = useLocation();
+  function handleClick() {
+    pushLocation(`/gif/${id}`);
+  }
+
   return (
     <Card className="border-0">
-      <a href={`/gif/${id}`}>
-        <Card.Img
-          variant="top"
-          type="button"
-          src={url}
-          alt={title}
-        />
-      </a>
+      <Card.Img
+        variant="top"
+        type="button"
+        src={url}
+        alt={title}
+        onClick={handleClick}
+      />
       <Card.Body className="GifCard-data rounded-bottom d-flex align-items-center justify-content-between p-1">
-        <Card.Title className="text-light fs-6 fw-normal text-truncate ps-1 m-0">
-          {title}
-        </Card.Title>
+        <Card.Title className="text-light fs-6 fw-normal text-truncate ps-1 m-0">{title}</Card.Title>
         {isInFavs ? (
           <BtnDeleteFromFavs gif={gif} />
         ) : (
